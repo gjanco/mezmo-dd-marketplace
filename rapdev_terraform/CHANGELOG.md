8204fbf48a98b21ef36cf2ea63c6d021549eb7cf
RapDev.io <39814076+rapdev@users.noreply.github.com>
2021-12-06 12:26:45 -0500

    Add better terraform error handling (#252)
    Co-authored-by: rapdev <tomas@rapdev.io>

--- rapdev_terraform/CHANGELOG.md
+++ rapdev_terraform/CHANGELOG.md
@@ -1,4 +1,7 @@
 # CHANGELOG - rapdev_terraform
 
+## 1.1.0 / 2021-11-16
+* [Added] Better error handling
+
 ## 1.0.0 / 2021-08-30
 * Initial Release
--- rapdev_terraform/datadog_checks/rapdev_terraform/terraform.py
+++ rapdev_terraform/datadog_checks/rapdev_terraform/terraform.py
@@ -32,7 +32,7 @@ class TerraformCheck(AgentCheck):
         self.metric_prefix = "rapdev.terraform"
         self.billing_metric = "{}.{}".format("datadog.marketplace", self.metric_prefix)
 
-        self.should_parse_agent_pools = self.instance.get("collect_agents")
+        self.should_parse_agent_pools = self.instance.get("collect_agents", False)
 
     def check(self, instance):
         self.validate_config()
@@ -98,12 +98,12 @@ class TerraformCheck(AgentCheck):
 
     def get_organization_ids(self):
         org_ids = []
-        organizations = self.call_api("organizations").get("data")
+        organizations = self.call_api("organizations").get("data", [])
 
         for org in organizations:
             metric_tags = self.tags.copy()
             org_id = org.get("id")
-            org_attributes = org.get("attributes")
+            org_attributes = org.get("attributes", {})
 
             org_ids.append(org_id)
             metric_tags.append("tf_org_id:{}".format(org_id))
@@ -125,7 +125,7 @@ class TerraformCheck(AgentCheck):
         parsed_users = set()
 
         while True:
-            teams = org_teams_response.get("data")
+            teams = org_teams_response.get("data", [])
 
             for team in teams:
                 metric_tags = self.tags.copy()
@@ -134,12 +134,12 @@ class TerraformCheck(AgentCheck):
                 team_id = team.get("id")
                 metric_tags.append("tf_team_id:{}".format(team_id))
 
-                team_attributes = team.get("attributes")
+                team_attributes = team.get("attributes", {})
                 metric_tags.append("tf_team_name:{}".format(team_attributes.get("name")))
                 metric_tags.append("tf_team_visibility:{}".format(team_attributes.get("visibility")))
 
                 # Team permissions
-                team_permissions = team_attributes.get("permissions")
+                team_permissions = team_attributes.get("permissions", {})
                 metric_tags.append("tf_team_perms_can_update_membership:{}".format(
                     team_permissions.get("can-update-membership")))
                 metric_tags.append("tf_team_perms_can_destroy:{}".format(
@@ -159,10 +159,10 @@ class TerraformCheck(AgentCheck):
                            team_attributes.get("users-count"),
                            tags=metric_tags)
 
-                users = team.get("relationships").get("users").get("data")
+                users = team.get("relationships", {}).get("users", {}).get("data", [])
                 parsed_users = self.get_user_information(users, parsed_users, org_id)
 
-            next_page_token = org_teams_response.get("meta").get("pagination").get("next-page")
+            next_page_token = org_teams_response.get("meta", {}).get("pagination", {}).get("next-page")
             if not next_page_token:
                 break
             else:
@@ -180,8 +180,8 @@ class TerraformCheck(AgentCheck):
                 parsed_users.add(user_id)
 
                 request_path = "users/{}".format(user_id)
-                user_data = self.call_api(request_path).get("data")
-                user_attributes = user_data.get("attributes")
+                user_data = self.call_api(request_path).get("data", {})
+                user_attributes = user_data.get("attributes", {})
 
                 metric_tags.append("tf_user_name:{}".format(user_attributes.get("username")))
                 metric_tags.append("tf_user_is_service_account:{}".format(
@@ -208,13 +208,13 @@ class TerraformCheck(AgentCheck):
         workspaces_response = self.call_api(request_path)
 
         while True:
-            workspaces = workspaces_response.get("data")
+            workspaces = workspaces_response.get("data", [])
 
             for workspace in workspaces:
                 metric_tags = self.tags.copy()
                 metric_tags.append("tf_org_id:{}".format(org_id))
 
-                attributes = workspace.get("attributes")
+                attributes = workspace.get("attributes", {})
 
                 workspace_name = attributes.get("name")
                 workspace_id = workspace.get("id")
@@ -237,7 +237,7 @@ class TerraformCheck(AgentCheck):
                            1,
                            tags=metric_tags)
 
-                latest_run_id = workspace.get("relationships").get("latest-run").get("data").get("id")
+                latest_run_id = workspace.get("relationships", {}).get("latest-run", {}).get("data", {}).get("id")
 
                 if latest_run_id:
                     self.get_latest_workspace_run(latest_run_id, workspace_name, workspace_id)
@@ -245,7 +245,7 @@ class TerraformCheck(AgentCheck):
                 # Get all the runs for this workspace
                 self.get_workspace_runs(workspace_id, workspace_name)
 
-            next_page_token = workspaces_response.get("meta").get("pagination").get("next-page")
+            next_page_token = workspaces_response.get("meta", {}).get("pagination", {}).get("next-page")
             if not next_page_token:
                 break
             else:
@@ -255,14 +255,14 @@ class TerraformCheck(AgentCheck):
         request_path = "runs/{}".format(latest_run_id)
         runs_response = self.call_api(request_path)
 
-        run = runs_response.get("data")
+        run = runs_response.get("data", {})
 
         metric_tags = self.tags.copy()
         metric_tags.append("tf_workspace_id:{}".format(workspace_id))
         metric_tags.append("tf_workspace_name:{}".format(workspace_name))
         metric_tags.append("tf_run_id:{}".format(latest_run_id))
 
-        run_attributes = run.get("attributes")
+        run_attributes = run.get("attributes", {})
         metric_tags.append("tf_run_source:{}".format(run_attributes.get("source")))
         metric_tags.append("tf_run_trigger_reason:{}".format(
             run_attributes.get("trigger-reason")))
@@ -310,7 +310,7 @@ class TerraformCheck(AgentCheck):
         runs_response = self.call_api(request_path)
 
         while True:
-            runs = runs_response.get("data")
+            runs = runs_response.get("data", [])
 
             for run in runs:
                 metric_tags = self.tags.copy()
@@ -318,7 +318,7 @@ class TerraformCheck(AgentCheck):
                 metric_tags.append("tf_workspace_name:{}".format(workspace_name))
                 metric_tags.append("tf_run_id:{}".format(run.get("id")))
 
-                run_attributes = run.get("attributes")
+                run_attributes = run.get("attributes", {})
                 metric_tags.append("tf_run_source:{}".format(run_attributes.get("source")))
                 metric_tags.append("tf_run_trigger_reason:{}".format(
                     run_attributes.get("trigger-reason")))
@@ -359,7 +359,7 @@ class TerraformCheck(AgentCheck):
                            status,
                            tags=metric_tags)
 
-            next_page_token = runs_response.get("meta").get("pagination").get("next-page")
+            next_page_token = runs_response.get("meta", {}).get("pagination", {}).get("next-page")
             if not next_page_token:
                 break
             else:
@@ -370,13 +370,13 @@ class TerraformCheck(AgentCheck):
         agent_pools_response = self.call_api(request_path)
 
         while True:
-            agent_pools = agent_pools_response.get("data")
+            agent_pools = agent_pools_response.get("data", [])
             for agent_pool in agent_pools:
                 metric_tags = self.tags.copy()
                 agent_pool_id = agent_pool.get("id")
                 metric_tags.append("tf_agent_pool_id:{}".format(agent_pool_id))
 
-                agent_pool_attributes = agent_pool.get("attributes")
+                agent_pool_attributes = agent_pool.get("attributes", {})
                 if agent_pool_attributes:
                     metric_tags.append("tf_agent_pool_name:{}".format(agent_pool_attributes.get("name")))
 
@@ -388,7 +388,7 @@ class TerraformCheck(AgentCheck):
 
                 self.get_agents(agent_pool_id, metric_tags)
 
-            next_page_token = agent_pools_response.get("meta").get("pagination").get("next-page")
+            next_page_token = agent_pools_response.get("meta", {}).get("pagination", {}).get("next-page")
             if not next_page_token:
                 break
             else:
@@ -399,19 +399,19 @@ class TerraformCheck(AgentCheck):
         auth_tokens_response = self.call_api(request_path)
 
         while True:
-            auth_tokens = auth_tokens_response.get("data")
+            auth_tokens = auth_tokens_response.get("data", [])
 
             for auth_token in auth_tokens:
                 token_tags = metric_tags.copy()
                 token_tags.append("tf_agent_auth_token_id:{}".format(auth_token.get("id")))
-                auth_token_creator = auth_token.get("relationships").get("created-by").get("data").get("id")
+                auth_token_creator = auth_token.get("relationships", {}).get("created-by", {}).get("data", {}).get("id")
                 token_tags.append("tf_agent_auth_token_creator:{}".format(auth_token_creator))
 
                 self.gauge("{}.org.agent_pool.auth_tokens.count".format(self.metric_prefix),
                            1,
                            tags=token_tags)
 
-            next_page_token = auth_tokens_response.get("meta").get("pagination").get("next-page")
+            next_page_token = auth_tokens_response.get("meta", {}).get("pagination", {}).get("next-page")
             if not next_page_token:
                 break
             else:
@@ -422,12 +422,12 @@ class TerraformCheck(AgentCheck):
         agents_in_pool_response = self.call_api(request_path)
 
         while True:
-            agents = agents_in_pool_response.get("data")
+            agents = agents_in_pool_response.get("data", [])
 
             for agent in agents:
                 agent_tags = metric_tags.copy()
 
-                agent_attributes = agent.get("attributes")
+                agent_attributes = agent.get("attributes", {})
 
                 agent_tags.append("tf_agent_name:{}".format(agent_attributes.get("name")))
                 agent_tags.append("tf_agent_ip:{}".format(agent_attributes.get("ip-address")))
@@ -453,7 +453,7 @@ class TerraformCheck(AgentCheck):
                            status,
                            tags=agent_tags)
 
-            next_page_token = agents_in_pool_response.get("meta").get("pagination").get("next-page")
+            next_page_token = agents_in_pool_response.get("meta", {}).get("pagination", {}).get("next-page")
             if not next_page_token:
                 break
             else:
@@ -461,13 +461,13 @@ class TerraformCheck(AgentCheck):
 
     def get_terraform_variables(self):
         request_path = "vars"
-        terraform_variables = self.call_api(request_path).get("data")
+        terraform_variables = self.call_api(request_path).get("data", [])
 
         for variable in terraform_variables:
             metric_tags = self.tags.copy()
             metric_tags.append(variable.get("id"))
 
-            variable_attributes = variable.get("attributes")
+            variable_attributes = variable.get("attributes", {})
 
             metric_tags.append("tf_variable_key:{}".format(variable_attributes.get("key")))
             metric_tags.append("tf_variable_is_sensitive:{}".format(variable_attributes.get("sensitive")))
