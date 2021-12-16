59edfe2d51dcdc36ce111f7e0ed33b1244aaa230
Owen Moreau <48562956+moreauowen@users.noreply.github.com>
2021-12-16 16:11:40 -0500

    Adjust marketplace media validations (#265)
    * Round to 2 decimal places instead of 4
    
    * Change validations to run when any file is changed for an integration
    
    * Add range to aspect ratio validation
    
    * Fix filename issue for validations
    
    * Add comment to clarify aspect ratio range

--- .github/workflows/scripts/v2-media-validations.py
+++ .github/workflows/scripts/v2-media-validations.py
@@ -6,23 +6,32 @@ import os
 import json
 
 # Requirement constants
-TARGET_ASPECT_RATIO = round(16 / 9, 4)
 MINIMUM_WIDTH = 1440
 MAXIMUM_WIDTH = 2880
 MEDIA_PATH = '/tile/media'
+# These values represent 16:9 aspect ratio (with tolerance)
+MIN_ASPECT_RATIO = 1.77
+MAX_ASPECT_RATIO = 1.78
 
 
-def get_check_name_from_manifest_path(file_path):
+def get_changed_integrations(changed_files):
     """
-    Helper function to get the check name based on a manifest.json path
+    Helper function takes in a list of changed files and returns a list of integrations where
+    files were changed. Validations will run for each integration found.
     """
-    parts = file_path.lstrip('/').split('/')
-    if len(parts) != 2:
-        return None
-    if parts[1] == 'manifest.json':
-        return parts[0]
-    else:
-        return None
+    names_to_exclude = (
+        '.azure-pipelines',
+        '.github',
+        '.gitlab',
+        '.in-toto',
+    )
+    check_name_to_validate = set()
+    for file in changed_files:
+        parts = file.filename.lstrip('/').split('/')
+        check_name = parts[0]
+        if check_name not in names_to_exclude:
+            check_name_to_validate.add(check_name)
+    return list(check_name_to_validate)
 
 
 def validate_media_elements(check_name, manifest_dict):
@@ -44,10 +53,10 @@ def validate_media_elements(check_name, manifest_dict):
         try:
             image = Image.open(image_path)
             width, height = image.size
-            aspect_ratio = round(width / height, 4)
-            if aspect_ratio != TARGET_ASPECT_RATIO:
+            aspect_ratio = round(width / height, 2)
+            if aspect_ratio < MIN_ASPECT_RATIO or aspect_ratio > MAX_ASPECT_RATIO:
                 output = (f'  The aspect ratio for the image at `{image_path}` is {aspect_ratio}, but should be 16:9 '
-                          f'({TARGET_ASPECT_RATIO}).')
+                          f'(1.78).')
                 error_output.append(output)
             if width < MINIMUM_WIDTH:
                 output = (f'  The width for the image at `{image_path}` should be a minimum of {MINIMUM_WIDTH}px, but '
@@ -66,37 +75,39 @@ def validate_media_elements(check_name, manifest_dict):
 
 def main():
     repo = Github(os.environ.get('GITHUB_TOKEN')).get_repo(os.environ.get('GITHUB_REPOSITORY'))
-
-    manifests_changed = []
+    checks_changed = []
     with open(os.environ['GITHUB_EVENT_PATH']) as event_file:
-        # Get the PR triggering the action
-        event = json.load(event_file)
-        pr_number = event['pull_request']['number']
-        pr = repo.get_pull(pr_number)
+        # Get the PR triggering the action, if not a PR, then stop validations
+        try:
+            event = json.load(event_file)
+            pr_number = event['pull_request']['number']
+            pr = repo.get_pull(pr_number)
+        except KeyError:
+            print('Stopping validations because this is not a PR.')
+            return
 
         # Check for changes to manifest.json files in this PR
-        for file in pr.get_files():
-            if 'manifest.json' in file.filename:
-                manifests_changed.append(file.filename)
+        changed_files = pr.get_files()
+        changed_integrations = get_changed_integrations(changed_files)
+        checks_changed.extend(changed_integrations)
 
     # Skip if no manifest files were changed in this PR
-    if not manifests_changed:
-        print(f'There are no manifest.json changes - skipping media validations!')
+    if not checks_changed:
+        print(f'There are no integration changes - skipping media validations!')
         return
 
     # For every changed manifest in this PR, validate the media elements listed in the file
     all_errors = []
-    for manifest_path in manifests_changed:
-        current_check_name = get_check_name_from_manifest_path(manifest_path)
-        if current_check_name:
-            try:
-                with open(manifest_path) as manifest_file:
-                    manifest_data = json.load(manifest_file)
-                    errors = validate_media_elements(current_check_name, manifest_data)
-                    all_errors.extend(errors)
-                    annotate_errors(manifest_path, errors)
-            except FileNotFoundError:
-                print(f'Could not find manifest file at this path: {manifest_path}')
+    for current_check_name in checks_changed:
+        manifest_path = f'{current_check_name}/manifest.json'
+        try:
+            with open(manifest_path) as manifest_file:
+                manifest_data = json.load(manifest_file)
+                errors = validate_media_elements(current_check_name, manifest_data)
+                all_errors.extend(errors)
+                annotate_errors(manifest_path, errors)
+        except FileNotFoundError:
+            print(f'Could not find manifest file at this path: {manifest_path}')
 
     # Print error output in addition to annotating
     if all_errors:
